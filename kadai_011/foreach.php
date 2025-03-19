d0f4dfb68c2aef0f4649968132895ff0c8beda5f
Lii0971 <kurumi.tamada@gmail.com>
2025-03-19 18:48:57 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+<p>
+        <?php
+        $personal_date = [
+            'name' => '玉ねぎ',
+            'price' => '200',
+            'location' => '北海道'
+        ];
+
+        // キーを日本語にマッピング
+        $key_mapping = [
+            'name' => '名前',
+            'price' => '値段',
+            'location' => '産地'
+        ];
+
+        foreach ($personal_date as $key => $value) {
+            // マッピングを用いて表示
+            echo "{$key_mapping[$key]}: {$value}. <br>";
+        }
+        ?>
+    </p>
+   </body>
+
+</html>
